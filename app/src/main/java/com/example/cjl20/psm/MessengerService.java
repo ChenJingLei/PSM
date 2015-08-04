79fc336417085f956f3b220b7b09705bb763ed65
ChenJingLei <cjl4565@163.com>
2015-08-04 17:17:00 +0800

    V1.0 Solve the some problems

--- app/src/main/java/com/example/cjl20/psm/MessengerService.java
+++ app/src/main/java/com/example/cjl20/psm/MessengerService.java
@@ -1,7 +1,5 @@
 package com.example.cjl20.psm;
 
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
